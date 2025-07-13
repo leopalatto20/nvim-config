bf5b644620317a992ada4f6ba7cc3d34687b3366
leopalatto20 <A01665462@tec.mx>
2025-07-13 09:55:08 -0600

    Cambios en snippets

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+> [!NOTE]
+> hola amigos como estan amigos
+
+- [x] hola
+- [x] fortnite
+- [-] text
+
+## hola amigos como estan amigos
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,45 +1,29 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
-  { "kdheepak/monochrome.nvim" },
   {
-    "idr4n/github-monochrome.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  { "sainnhe/edge" },
-  { "rebelot/kanagawa.nvim" },
-  { "EdenEast/nightfox.nvim" },
-  { "rose-pine/neovim" },
-  {
-    "LazyVim/LazyVim",
+    "rose-pine/neovim",
+    name = "rose-pine",
     opts = {
-      colorscheme = "rose-pine",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
   {
-    "wnkz/monoglow.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "modus_vivendi",
+    },
   },
   {
-    "zenbones-theme/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = "rktjmp/lush.nvim",
+    "uloco/bluloco.nvim",
     lazy = false,
     priority = 1000,
-    -- you can set set configuration options here
-    -- config = function()
-    --     vim.g.zenbones_darken_comments = 45
-    --     vim.cmd.colorscheme('zenbones')
-    -- end
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function()
+      -- your optional config goes here, see below.
+    end,
   },
-  {
-    "metalelf0/black-metal-theme-neovim",
-    lazy = false,
-    priority = 1000,
-  },
+  { "miikanissi/modus-themes.nvim", priority = 1000 },
 }
